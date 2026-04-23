e74223654bf60e2f3c3300d84be86f9cb8b054ef
JohanW23M <jawang@utpl.edu.ec>
2026-04-23 10:52:21 -0500

    Parte 1: Implementación con extends Thread

--- src/main/java/ec/edu/utpl/carreras/computacion/proava/MultiTimerConsole.java
+++ src/main/java/ec/edu/utpl/carreras/computacion/proava/MultiTimerConsole.java
@@ -1,12 +1,16 @@
 package ec.edu.utpl.carreras.computacion.proava;
 
-/**
- * Hello world!
- */
 public class MultiTimerConsole {
+
     public static void main(String[] args) {
         TimerTask timerA = new TimerTask("Temporizador A", 8);
-        timerA.run();
-        System.out.println();
+        TimerTask timerB = new TimerTask("Temporizador B", 5);
+        TimerTask timerC = new TimerTask("Temporizador C", 3);
+
+        timerA.start();
+        timerB.start();
+        timerC.start();
+
+        System.out.println(">>> Los hilos han sido iniciados...");
     }
 }
